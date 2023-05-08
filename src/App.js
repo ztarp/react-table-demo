c6f62648804a25cf8dce6a9bfe0059823de6ffeb
AzureAD\ZacharyTarpley <zac.tarpley@gmail.com>
2023-05-08 18:25:04 -0400

    Keeping this instance for the comments.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,90 @@
 // App.js
 
 import axios from "axios";
-import { useState, useEffect } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 
 import Table from "./Table";
 import "./App.css";
 
+  // Custom component to render Genres 
+const Genres = ({ values }) => {
+  // Loop through the array and create a badge-like component instead of a comma-separated string
+  return (
+    <>
+      {values.map((genre, idx) => {
+        return (
+          <span key={idx} className="badge">
+            {genre}
+          </span>
+        );
+      })}
+    </>
+  );
+};
+
 function App() {
+  /* 
+    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+  */
+
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        Header: "TV Show",
+        // First group columns
+        columns: [
+          {
+            Header: "Name",
+            accessor: "show.name",
+          },
+          {
+            Header: "Type",
+            accessor: "show.type",
+          },
+        ],
+      },
+      {
+        // Second group - Details
+        Header: "Details",
+        // Second group columns
+        columns: [
+          {
+            Header: "Language",
+            accessor: "show.language",
+          },
+          {
+            Header: "Genre(s)",
+            accessor: "show.genres",
+            // Cell method will provide the cell value; we pass it to render a custom component
+            Cell: ({ cell: { value } }) => <Genres values={value} />
+          },
+          {
+            Header: "Runtime",
+            accessor: "show.runtime",
+            // Cell method will provide the value of the cell; we can create a custom element for the Cell        
+            Cell: ({ cell: { value } }) => {
+              const hour = Math.floor(value / 60);
+              const min = Math.floor(value % 60);
+              return (
+                <>
+                  {hour > 0 ? `${hour} hr${hour > 1 ? "s" : ""} ` : ""}
+                  {min > 0 ? `${min} min${min > 1 ? "s" : ""}` : ""}
+                </>
+              );
+            }
+          },
+          {
+            Header: "Status",
+            accessor: "show.status",
+          },
+        ],
+      },
+    ],
+    []
+  );
+
   // data state to store the TV Maze API data. Its initial value is an empty array
   const [data, setData] = useState([]);
 
@@ -18,6 +96,10 @@ function App() {
     })();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Table columns={columns} data={data} />
+    </div>
+  );
 }
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -1,3 +1,84 @@
-export default function Table({columns, data}) {
-    // Table component and UI come here
+// Table.js
+
+import React, { useState } from "react";
+import { useTable, useFilters, useSortBy } from "react-table";
+import "./table.css"
+
+export default function Table({ columns, data }) {
+  // Use the useTable Hook to send the columns and data to build the table
+  const {
+    getTableProps, // table props from react-table
+    getTableBodyProps, // table body props from react-table
+    headerGroups, // headerGroups, if your table has groupings
+    rows, // rows for the table based on the data passed
+    prepareRow, // Prepare the row (this function needs to be called for each row before getting the row props)
+    setFilter, // The useFilter Hook provides a way to set the filter
+  } = useTable(
+    {
+        columns,
+        data
+    },
+    useFilters,  // Adding the useFilters Hook to the table
+    // You can add as many Hooks as you want. Check the documentation for details. You can even add custom Hooks for react-table here
+    useSortBy // This Plugin Hook will help us sort our columns
+  );
+
+  const [ filterInput, setFilterInput ] = useState("");
+
+  // Update the state when input changes
+    const handleFilterChange = e => {
+        const value = e.target.value || null;
+        setFilter("show.name", value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
+        setFilterInput(value);
+    };
+
+  /* 
+    Render the UI for your table
+    - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
+
+    Table header styling and props to allow sorting
+  */
+  return (
+    <>
+        <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={"Search name"}
+        />
+        <table {...getTableProps()}>
+        <thead>
+            {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                <th
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    className={
+                    column.isSorted
+                        ? column.isSortedDesc
+                        ? "sort-desc"
+                        : "sort-asc"
+                        : ""
+                    }
+                >
+                    {column.render("Header")}
+                </th>
+                ))}
+            </tr>
+            ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+                <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                    return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                })}
+                </tr>
+            );
+            })}
+        </tbody>
+        </table>
+    </>
+  );
 }
