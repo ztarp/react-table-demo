c13eb17ee8122eaa9fe5aa964ce09407ed009acf
AzureAD\ZacharyTarpley <zac.tarpley@gmail.com>
2023-05-08 16:31:17 -0400

    Added axios, react-table, and Table.js file.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import Table from "./Table";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  // data state to store the TV Maze API data. Its initial value is an empty array
+  const [data, setData] = useState([]);
 
+  // Using useEffect to call the API once mounted and set the data
+  useEffect(() => {
+    (async () => {
+      const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
+      setData(result.data);
+    })();
+  }, []);
+
+  return <div className="App"></div>;
+}
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,3 @@
+export default function Table({columns, data}) {
+    // Table component and UI come here
+}
